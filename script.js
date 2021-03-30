796c74967b3a4477c3d7b0554287be291f1c74fb
Charliemur2 <charliemurciamunoz@gmail.com>
2021-03-29 23:22:02 -0500

    first basic version of dsrum-kit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /* getting keydown event and sound */
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  audio.currenTime = 0;
   if(!audio) return;
   audio.play();
 });
 window.addEventListener('click', function(e) {
-  /* const drumKit = document.querySelector('.drum-kit');
-  const drumKitParts = drumKit.getElementsByTagName('div'); */
   const drumPart= e.srcElement.className;
   const drumPartInstrument = document.querySelector(`.${drumPart}`);
   const dataKey = drumPartInstrument.dataset.key;
